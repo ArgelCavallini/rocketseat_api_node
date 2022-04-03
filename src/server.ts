5a54f6ec61dbfc5132cf526756a645470c663eb2
Argel Cavallini <argel_095@hotmail.com>
2022-04-03 14:19:58 -0300

    coonfiguracoes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+});
+
 app.listen(3333, () => {
-    console.log("deu boa...")
-})
+    console.log("deu boa...");
+});
