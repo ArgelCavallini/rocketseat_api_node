33f907519e29fd4b012e85c08cb60256b7cc5326
argelcavallini <argel_095@hotmail.com>
2022-05-18 21:43:31 -0300

    alterações

--- src/routes/Categories.routes.ts
+++ src/routes/Categories.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { CreateCategoryService } from '../services/CreateCategoryService';
+
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
@@ -8,13 +10,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category Already exists!' });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  
+  createCategoryService.execute({ name, description });
 
   return response.status(200).send();
 });
