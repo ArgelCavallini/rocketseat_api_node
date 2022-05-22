dadd118bc1cdff39ceafb26fe0c3758e74af24ac
argelcavallini <argel_095@hotmail.com>
2022-05-22 20:07:02 -0300

    upload

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,20 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs"; // file system nativo do node
+
+
+class ImportCategoryUseCase {
+    execute(file: Express.Multer.File):void {
+        
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
+    }
+}
+
+export { ImportCategoryUseCase}
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 
-import { SpecificatiosRepository } from '../modules/cars/repositories/implementations/SpecificatiosRepository';
 import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificatiosRepository();
 
 specificationsRoutes.post('/', (request, response) => {
   return createSpecificationController.handle(request, response)
